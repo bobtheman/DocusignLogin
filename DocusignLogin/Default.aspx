f909af2d7c5e9b073aeec65dce72dca6710cf30f
bobtheman <rob_owen@outlook.com>
2018-08-30 11:21:01 +0100

    Add files via upload

---
+++ DocusignLogin/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DocusignLogin.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel runat="server">
+            <ContentTemplate>
+                <div>
+                    <table style="width: 500px;">
+                        <tr>
+                            <td>Email Address</td>
+                            <td>
+                                <asp:TextBox runat="server" ID="txtemail" TextMode="Email" Width="99%" ValidationGroup="ValLogin"></asp:TextBox>
+                            </td>
+                            <td><asp:RequiredFieldValidator runat="server" ID="valemail" ControlToValidate="txtemail" SetFocusOnError="true" ErrorMessage="*" ForeColor="Red" ValidationGroup="ValLogin"></asp:RequiredFieldValidator></td>
+                        </tr>
+                        <tr>
+                            <td>Password</td>
+                            <td>
+                                <asp:TextBox runat="server" ID="txtpassword" TextMode="Password" Width="99%" ValidationGroup="ValLogin"></asp:TextBox>
+                            </td>
+                            <td><asp:RequiredFieldValidator runat="server" ID="valpassword" ControlToValidate="txtpassword" SetFocusOnError="true" ErrorMessage="*" ForeColor="Red" ValidationGroup="ValLogin"></asp:RequiredFieldValidator></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <asp:Button runat="server" ID="btnproceed" OnClick="btnproceed_Click" Text="Proceed" Width="100%" ValidationGroup="ValLogin" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <asp:Label runat="server" ID="lblSuccess" Text="Login Success" Visible="false" BackColor="Green" Width="100%"></asp:Label>
+                                <asp:Label runat="server" ID="lblfailure" Visible="false" BackColor="Red" Width="100%"></asp:Label>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ DocusignLogin/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace DocusignLogin
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public const string IntegratorKey = "YourIntegratorKey";
+        public const string PostmanKey = "YourPostmanKey";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnproceed_Click(object sender, EventArgs e)
+        {
+            string username = txtemail.Text.Trim();
+            string password = txtpassword.Text.Trim();
+            var client = new RestClient("https://www.docusign.net/restapi/v2/login_information?api_password=true");
+            var request = new RestRequest(Method.GET);
+            //PostMan Key//
+            //This is primarily used to bypass a bug in Chrome. If an XMLHttpRequest is pending and another request is sent with the//
+            //same parameters then Chrome returns the same response for both of them. Sending a random token avoids this issue.//
+            //This can also help you distinguish between request on the server side.//
+            request.AddHeader("postman-token", PostmanKey); 
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("x-docusign-authentication", "{\"Username\":\"" + username  + "\",\"Password\":\"" + password + "\",\"IntegratorKey\": \"" + IntegratorKey + "\"}");
+            IRestResponse response = client.Execute(request);
+
+            RootObject root = JsonConvert.DeserializeObject<RootObject>(response.Content);
+
+            if (string.IsNullOrEmpty(root.errorCode))
+            {
+                lblSuccess.Visible = true;
+                lblfailure.Visible = false;
+                return;
+            }
+
+            lblfailure.Text = root.message;
+            lblfailure.Visible = true;
+            lblSuccess.Visible = false;
+        }
+
+        public class LoginAccount
+        {
+            public string name { get; set; }
+            public string accountId { get; set; }
+            public string baseUrl { get; set; }
+            public string isDefault { get; set; }
+            public string userName { get; set; }
+            public string userId { get; set; }
+            public string email { get; set; }
+            public string siteDescription { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<LoginAccount> loginAccounts { get; set; }
+            public string apiPassword { get; set; }
+            public string errorCode { get; set; }
+            public string message { get; set; }
+        }
+    }
+}
